b2d23f0d7dfc035c3b2c4905fbda68299c630547
Hady Dev Center <yasikaekki07@gmail.com>
2021-07-19 17:31:22 +0700

    second commit

--- js/info.js
+++ js/info.js
@@ -1,22 +1,22 @@
 var nama = prompt("Inputkan nama anda : ");
 
 if(nama != ""){
-    window.alert("Selamat datang "+nama+"\nAnda pengunjung ke-"+parseInt(5*3+2)+" pada situs kami");
+    window.alert("Selamat datang "+nama+"\nAnda pengunjung ke-"+parseInt(5*3+2)+" pada web saya");
 } else {
-    window.alert("Selamat datang "+nama+"\nAnda pengunjung ke-"+parseInt(5*3+2)+" pada situs kami");
+    window.alert("Selamat datang "+nama+"\nAnda pengunjung ke-"+parseInt(5*3+2)+" pada web saya");
 }
 
 function isian() {
     if(nama != ""){
-        window.alert("Selamat datang "+nama+"\nAnda pengunjung ke-"+parseInt(5*3+2)+" pada situs kami");
+        window.alert("Selamat datang "+nama+"\nAnda pengunjung ke-"+parseInt(5*3+2)+" pada web saya");
     } else {
         window.alert("Maaf anda tidak menginputkan data apapun :(");
     }
 } 
 
 function iniConfirm() {
-    if (window.confirm("Apa anda tertarik dengan situs kami ?")) {
-        window.alert("Terima Kasih sudah support situs kami ^_^")
+    if (window.confirm("Apa anda tertarik dengan web saya ?")) {
+        window.alert("Terima Kasih sudah support web saya ^_^")
     } else {
         window.alert("Ok, lain kali ya :D")
     }
